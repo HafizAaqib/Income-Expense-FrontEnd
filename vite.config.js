5bfff23218590b998e1f0a8983f1f1bd434484bd
aaqib <aaqib@finexcloud.com>
2025-08-05 08:36:20 +0500

    Update code

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api/v1': {
-        target: 'http://localhost:8080',
+        target: 'https://income-expense-backend.on.shiper.app',
         changeOrigin: true,
       },
     },
